d409ade8d4945f4d9a780cd513b90062d6a8396c
mcrayven <bautista_mcrayven@spcc.edu.ph>
2026-02-19 16:56:41 +0800

    dsfds

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,6 +6,13 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
     }
 
     public void QuitGame()
